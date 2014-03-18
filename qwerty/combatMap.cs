a036184a5b00a6842d025704a7b4009f23b00dc3
destacio <destaciojl@gmail.com>
2014-03-18 12:42:34 +0700

    one tho three go

---
+++ qwerty/combatMap.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qwerty
+{
+    class combatMap
+    {
+        public int width;
+        public int height;
+        public int scale = 3;
+        public List<Box> boxes = new List<Box>();
+        public int deltax; 
+        public int deltay; 
+        public combatMap(int w, int h) 
+        {
+            width = w;
+            height = h;
+
+            deltax = 15 * scale;
+            deltay = 20 * scale;
+
+            iniBasicPoints();
+        }
+        public void iniBasicPoints()
+        {
+            int xcoord = -1;
+            int ycoord = 0;
+            for(int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (j % height == 0) xcoord += 1;
+
+                    if(i % 2 == 1)
+                    {
+                        // нечетная
+                        if (j % height == 0) ycoord = 1;
+                        Box box = new Box(scale);
+                        box.xmove(deltax * i - 10 * scale);
+                        box.ymove(deltay * j + 10 * scale);
+                        box.x = xcoord;
+                        box.y = ycoord;
+
+                        boxes.Add(box);
+
+                    }
+                    else
+                    {
+                        // четная
+                        if (j % height == 0) ycoord = 0;
+                        Box box = new Box(scale);
+                        box.xmove(deltax * i - 10 * scale);
+                        box.ymove(deltay * j + 0);
+                        box.x = xcoord;
+                        box.y = ycoord;
+
+                        boxes.Add(box);
+                    }
+                    ycoord += 2;
+                }
+            }
+        }
+    }
+
+ 
+}
